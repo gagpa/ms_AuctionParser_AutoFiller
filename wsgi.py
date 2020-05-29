ed2b6c8858aa47413da83d3848efa1a9e3c9e63b
gagpa <speitshot@gmail.com>
2020-05-29 20:55:33 +0300

    Убран flask shell

--- wsgi.py
+++ wsgi.py
@@ -11,10 +11,3 @@ from app import create_app
 app = create_app(os.environ.get('APP_MODE') or 'default')
 if __name__ == '__main__':
     app.run()
-
-
-from app import db
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db)
